410b2fcae4b541923d69d6b0fc37155bb8524690
EnergeticOAK630 <99870026+EnergeticOAK630@users.noreply.github.com>
2024-11-01 21:42:09 -0300

    Add files via upload

---
+++ HTML.CSS/Aula9/lucas.py
@@ -0,0 +1,13 @@
+n1 = float(input("digite um número"))
+n2 = float(input("digite outro npumero"))
+
+total =  n1 + n2
+
+print(total)
+
+if total >= 50:
+    print("maior ou igual")
+else:
+    print("menor")
+
+    
